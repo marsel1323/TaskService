a652ca77253c254934de860f6ad0e40780c8e30d
mabdrakhmanov <mabdrakhmanov@bekitzur.com>
2019-10-25 18:08:02 +0300

    implemented db methods

--- src/services/index.js
+++ src/services/index.js
@@ -1,18 +1,16 @@
 const { Task } = require('../models');
 
+
 const get = ({ id }) => Task.query().findById(id);
 
 const create = ({ name }) => Task.query().insert({ name, status: 'new' });
 
 const list = ({ limit, offset }) => Task.query().select('*').offset(offset || 0).limit(limit || 10);
 
-const update = (task) => {
+const update = ({ id, ...creds }) => Task.query().updateAndFetchById(id, creds);
 
-};
+const remove = ({ id }) => Task.query().deleteById(id);
 
-const remove = (id) => {
-
-};
 
 module.exports = {
   create,
